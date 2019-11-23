f03aa99766a38945ea9d540883ec3e2dbbbde320
git <marlonergondossantos@gmail.com>
2019-11-23 14:48:49 -0200

    testing app and add http client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'ErgonPictures';
+  title = "ErgonPictures";
+  photos = [
+    {
+      url:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDUJKHpJLOsJ110MmawBB389dthgYkHkbtflaiV6dnT3hww6dG",
+      description: "testando1"
+    },
+    {
+      url:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQDUJKHpJLOsJ110MmawBB389dthgYkHkbtflaiV6dnT3hww6dG",
+      description: "testando5"
+    }
+  ];
+  constructor(http: HttpClient) {
+    console.log(http);
+  }
 }
