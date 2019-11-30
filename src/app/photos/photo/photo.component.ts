e488416e144f9430ff106c3ba1aa3ff63b7576ab
git <marlonergondossantos@gmail.com>
2019-11-30 12:40:20 -0200

    changed doble quotes to single quotes

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: "app-photo",
-  templateUrl: "./photo.component.html",
-  styleUrls: ["./photo.component.scss"]
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.scss']
 })
 export class PhotoComponent {
-  @Input() url = "";
-  @Input() description = "";
+  @Input() url = '';
+  @Input() description = '';
 }
