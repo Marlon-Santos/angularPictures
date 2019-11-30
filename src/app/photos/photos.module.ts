9771b9e14e4b7fad6fc60321350c5336ff3fbaf7
git <marlonergondossantos@gmail.com>
2019-11-30 12:37:45 -0200

    add filterPhotos pipe in declarations

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,8 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
+import { FilterPhotosPipe } from './photo-list/filterPhotos.pipe';
+import { PhotosComponent } from './photo-list/photos/photos.component';
+
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent],
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    FilterPhotosPipe,
+    PhotosComponent
+  ],
   imports: [HttpClientModule, CommonModule]
 })
 export class PhotosModule {}
