6918e5d72157bc64fae85a916be44ace3267b41e
git <marlonergondossantos@gmail.com>
2019-12-07 09:57:49 -0200

    add resolve method in user route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,9 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { AppComponent } from './app.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 const routes: Routes = [
   { path: '', component: AppComponent },
-  { path: 'user/:userName', component: PhotoListComponent },
+  {
+    path: 'user/:userName',
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResolver
+    }
+  },
   { path: '**', component: NotFoundComponent }
 ];
 
