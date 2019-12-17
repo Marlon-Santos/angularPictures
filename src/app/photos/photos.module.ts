122070566fa7838ca38cf059a9101716add4c4ed
git <marlonergondossantos@gmail.com>
2019-12-17 12:28:40 -0200

    declarate btnComponent in photosModule

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,13 +6,15 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
 import { FilterPhotosPipe } from './photo-list/filterPhotos.pipe';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { MorePhotosBtnComponent } from './photo-list/more-photos-btn/more-photos-btn.component';
 
 @NgModule({
   declarations: [
     PhotoComponent,
     PhotoListComponent,
     FilterPhotosPipe,
-    PhotosComponent
+    PhotosComponent,
+    MorePhotosBtnComponent
   ],
   imports: [HttpClientModule, CommonModule]
 })
