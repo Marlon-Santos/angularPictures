37a1fb7719b22c4611ce488ee85d32bb8a08ba35
git <marlonergondossantos@gmail.com>
2019-12-17 12:31:14 -0200

    create singin component

---
+++ src/app/form/sing-in/sing-in.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './sing-in.component.html',
+  styleUrls: ['./sing-in.component.scss']
+})
+export class SingInComponent {}
