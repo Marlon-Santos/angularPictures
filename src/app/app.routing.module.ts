42815aa9f7e518cd2a00b78b47fe55818e00ca31
git <marlonergondossantos@gmail.com>
2019-12-17 12:32:30 -0200

    create new route for singIn

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { AppComponent } from './app.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { SingInComponent } from './form/sing-in/sing-in.component';
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: SingInComponent },
   {
     path: 'user/:userName',
     component: PhotoListComponent,
