c515a3ddf601c87648216b688f3be63e9f31940b
git <marlonergondossantos@gmail.com>
2019-12-17 12:31:39 -0200

    create new module (form)

---
+++ src/app/form/form.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { SingInComponent } from './sing-in/sing-in.component';
+
+@NgModule({
+  declarations: [SingInComponent]
+})
+export class FormModule {}
