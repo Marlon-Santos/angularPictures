5ad799e0cafad569e53a4f5966804c908a2da85d
git <marlonergondossantos@gmail.com>
2019-12-23 14:49:53 -0200

    add loginForm and formBuilder

--- src/app/form/sing-in/sing-in.component.ts
+++ src/app/form/sing-in/sing-in.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './sing-in.component.html',
   styleUrls: ['./sing-in.component.scss']
 })
-export class SingInComponent {}
+export class SingInComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+}
