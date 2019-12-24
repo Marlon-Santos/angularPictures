2eea3bf066e542804b5a73569bf3af45d988fece
git <marlonergondossantos@gmail.com>
2019-12-24 13:05:34 -0200

    add singIn method with auth service

--- src/app/form/sing-in/sing-in.component.ts
+++ src/app/form/sing-in/sing-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './sing-in.component.html',
@@ -7,11 +9,30 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class SingInComponent implements OnInit {
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+  singIn() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.auth.authenticate(userName, password).subscribe(
+      sucess => {
+        alert('logado com sucesso');
+        this.router.navigate(['user', userName]);
+      },
+      err => {
+        alert('usuario não autorizado');
+        this.loginForm.reset();
+      }
+    );
   }
 }
