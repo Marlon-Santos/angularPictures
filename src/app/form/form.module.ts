632484231b806ee6945683e2019a26832200f64b
git <marlonergondossantos@gmail.com>
2019-12-24 13:03:54 -0200

    import router module

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { SingInComponent } from './sing-in/sing-in.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [SingInComponent],
-  imports: [CommonModule, ReactiveFormsModule]
+  imports: [CommonModule, ReactiveFormsModule, RouterModule]
 })
 export class FormModule {}
