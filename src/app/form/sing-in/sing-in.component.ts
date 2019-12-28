2e50b09d6b62d9d1a7acd17be40276ea3e6913b2
git <marlonergondossantos@gmail.com>
2019-12-28 12:55:19 -0200

    create autoFoucus Element Ref

--- src/app/form/sing-in/sing-in.component.ts
+++ src/app/form/sing-in/sing-in.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/auth.service';
 import { Router } from '@angular/router';
+import { PlataformService } from 'src/app/core/plataform/plataform.service';
 
 @Component({
   templateUrl: './sing-in.component.html',
   styleUrls: ['./sing-in.component.scss']
 })
 export class SingInComponent implements OnInit {
+  @ViewChild('autoFocus', { static: false })
+  autoFocus: ElementRef<HTMLInputElement>;
   loginForm: FormGroup;
   constructor(
     private formBuilder: FormBuilder,
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private plataformService: PlataformService
   ) {}
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -32,6 +36,9 @@ export class SingInComponent implements OnInit {
       err => {
         alert('usuario não autorizado');
         this.loginForm.reset();
+        // tslint:disable-next-line: no-unused-expression
+        this.plataformService.isPlataformBrowser() &&
+          this.autoFocus.nativeElement.focus();
       }
     );
   }
