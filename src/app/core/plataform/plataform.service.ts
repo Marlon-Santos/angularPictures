1b63ad2670b461e341c0e1d56d7f7a694d8e8ec9
git <marlonergondossantos@gmail.com>
2019-12-28 12:53:37 -0200

    create plataform service

---
+++ src/app/core/plataform/plataform.service.ts
@@ -0,0 +1,9 @@
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+@Injectable({ providedIn: 'root' })
+export class PlataformService {
+  constructor(@Inject(PLATFORM_ID) private plataformId: string) {}
+  isPlataformBrowser() {
+    return isPlatformBrowser(this.plataformId);
+  }
+}
